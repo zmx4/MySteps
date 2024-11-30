5992fa7666d06b69bf6ba350432c8f9a046ae08a
TickZhong <1601858426@qq.com>
2024-11-30 15:51:27 +0800

    20241130

---
+++ PTA/7-9 星战1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) 
+{
+    while (b != 0) 
+    {
+        int c = b;
+        b = a % b;
+        a = c;
+    }
+    return a;
+}
+
+int a[1000][1000] = {0};
+
+signed main() 
+{
+    int n, x, y;
+    cin >> n >> x >> y;
+    x += 2000; y += 2000;
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) 
+    {
+        int xx, yy;
+        cin >> xx >> yy;
+        xx += 2000; yy += 2000;
+        int dx = xx - x;
+        int dy = yy - y;
+        int u = gcd(abs(dx), abs(dy));
+        if (u == 0) u = 1;
+        dx /= u;
+        dy /= u;
+        if (a[dx + 2000][dy + 2000] == 0) 
+        {
+            cnt++;
+        }
+        a[dx + 2000][dy + 2000]++;
+    }
+
+    cout << cnt << endl;
+    return 0;
+}
