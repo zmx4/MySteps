a1c4515f3f0d8606b65801132282559e570bc8d0
TickZhong <1601858426@qq.com>
2024-11-30 15:23:01 +0800

    20241130

--- PTA/7-8 降维.cpp
+++ PTA/7-8 降维.cpp
@@ -1,9 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 int a[2048][2048];
+string ans = "";
+int n;
+void solve(const int &aa,const int &bb,const int &xx,const int &yy)
+{
+    cout<<aa<<bb<<xx<<yy<<endl;
+    if(xx-yy==1)
+    {
+        ans+=to_string(a[xx][yy]);
+        ans+=to_string(a[xx][yy+1]);
+        ans+=to_string(a[xx+1][yy]);
+        ans+=to_string(a[xx+1][yy+1]);
+    }
+    if(xx - aa == 1 && yy - bb == 1)
+    {
+        solve(1,1,xx/2,yy/2);
+        solve((xx/2)+1,(yy/2)+1,xx,yy);
+        solve(1+(xx/2),1,1,yy/2);
+        solve(1,1+(yy/2),xx/2,1);
+    }
+}
 signed main()
 {
-    int n;cin>>n;
+    cin>>n;
     //n++;
     for(int x = 1;x <= n;x++)
     {
@@ -14,15 +34,6 @@ signed main()
     }
     
     string ans = "";
-    for(int x = 1;x<=n;x+=2)
-    {
-        for(int y = 1;y <= n;y+=2)
-        {
-            ans+=to_string(a[x][y]);
-            ans+=to_string(a[x][y+1]);
-            ans+=to_string(a[x+1][y]);
-            ans+=to_string(a[x+1][y+1]);
-        }
-    }
+    solve(1,1,n,n);
     cout<<ans;
 }
