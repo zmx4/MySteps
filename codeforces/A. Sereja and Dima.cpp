f902f879f07147500593f91a724c7e54d1975245
TickZhong <1601858426@qq.com>
2024-12-14 21:42:07 +0800

    codeforces/A. Sereja and Dima.cpp

--- codeforces/A. Sereja and Dima.cpp
+++ codeforces/A. Sereja and Dima.cpp
@@ -13,6 +13,29 @@ signed main()
 	for(int i=0;i<n;++i)
 		cin>>a[i];
 	int s=0,d=0;
-	
+	int l = 0, r = n-1;
+	bool flag = true;
+	for (int i = 0; i < n; ++i)
+	{
+		if(a[l] > a[r])
+		{
+			if (flag)
+				s += a[l];
+			else
+				d += a[l];
+			l++;
+			flag= !flag;
+		}
+		else
+		{
+			if (flag)
+				s += a[r];
+			else
+				d += a[r];
+			r--;
+			flag=!flag;
+		}
+	}
+	cout<<s<<" "<<d<<endl;
 	return 0;
 }
