e1b9857d691696f38c026dd0ddfac48a64335faf
TickZhong <1601858426@qq.com>
2024-12-27 09:28:31 +0800

    默写常用函数

--- main.cpp
+++ main.cpp
@@ -1,18 +1,57 @@
 #include<iostream>
 #include <vector>
 using namespace std;
-int main()
+int GCD1(int a,int b)
 {
-	int i = 2;
-	switch (i)
+	while(b!=0)
 	{
-	case 1:
-		cout << i;
-		break;
-	case 2:
-		cout << i;
-		break;
-	default:
-		cout<<i;
+		int r=a%b;
+		a=b;
+		b=r;
 	}
+	return a;
+}
+
+int GCD2(int a,int b)
+{
+	if(b==0)
+		return a;
+	else
+		return GCD2(b,a%b);
+}
+
+int lcm(int a, int b)
+{
+	return a*b / GCD(a, b);
+}
+
+void insertsort(int a[], int n)
+{
+	for (int j = 1; j < n;j++)
+	{
+		int key = a[j];
+		int i = j - 1;
+		while(i>=0&&a[i]>key)
+		{
+			a[i + 1] = a[i];
+			i--;
+		}
+		a[i + 1] = key;
+	}
+}
+
+char *StrStr(const char *txt, const char *pat)
+{
+	if(pat[0]=='\0')return NULL;
+	int n = strlen(txt);
+	int m = strlen(pat);
+	for(int i = 0;i<=n-m;i++)
+	{
+		int j = 0;
+		while(j<m&&txt[i+j]==pat[j])
+			j++;
+		if(j==m)
+			return (char*)txt+i;
+	}
+	return NULL;
 }
