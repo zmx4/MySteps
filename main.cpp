518147fdb784b577569fb306b963b6c28ba8648f
TickZhong <1601858426@qq.com>
2024-12-27 09:30:51 +0800

    isPrime

--- main.cpp
+++ main.cpp
@@ -58,5 +58,13 @@ char *StrStr(const char *txt, const char *pat)
 
 bool isPrime(int n)
 {
-	if(n<)
+	if(n<=1)return false;
+	if(n==2)return true;
+	if(n%2==0)return false;
+	for(int i = 3;i*i<=n;i+=2)
+	{
+		if(n%i==0)
+			return false;
+	}
+	return true;
 }
