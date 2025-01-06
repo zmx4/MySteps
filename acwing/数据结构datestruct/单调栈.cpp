24ab1598c663f35e8c7713326a1d485c95489389
TickZhong <1601858426@qq.com>
2025-01-06 15:56:04 +0800

    单调栈

--- acwing/数据结构datestruct/单调栈.cpp
+++ acwing/数据结构datestruct/单调栈.cpp
@@ -2,13 +2,26 @@
 #define endl "\n"
 #define ll long long
 using namespace std;
+const int N = 100010;
+int n;
+int skt[N],top;
 
 signed main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(nullptr);cout.tie(nullptr);
+	cin.tie(nullptr);
 
-
+	cin>>n;
+	for(int i = 0;i < n;i++)
+	{
+		int x;
+		cin>>x;
+		while(top && skt[top] >= x) top--;
+		if(top)cout<<skt[top]<<" ";
+		else cout<<"-1 ";
+		//else cout<<"-1 "<<endl;
+		skt[++top] = x;
+	}
 
 	return 0;
 }
