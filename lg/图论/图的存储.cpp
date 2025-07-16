56675a64779e59f99030ad952a024986c293aa54
Tick Zhong <1601858426@qq.com>
2025-07-16 08:59:00 +0800

    添加多个新算法实现，包括扫雷、供电、拉力、训练、前缀和、图的存储和差分模板

---
+++ lg/图论/图的存储.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define gcd __gcd
+typedef int64_t i64;
+typedef int32_t i32;
+using ll = long long;using ull = unsigned long long;using LL = long long;using ULL = unsigned long long;
+using namespace std;
+const int inf = 0x3f3f3f3f,INF = 0x3f3f3f3f;
+#define int long long
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){os << "[";for (auto it = v.begin(); it != v.end(); ++it){if (it != v.begin())os << ", ";os << *it;}return os << "]";}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+#define dbg(...) cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = ", _dbg(__VA_ARGS__), cerr << endl
+void _dbg() {}
+template <typename T, typename... Args>
+void _dbg(T &&arg, Args &&...args){cerr << arg;if (sizeof...(args) > 0)cerr << ", ";_dbg(args...);}
+template <typename T, typename N, typename... Args>
+void _dbg(T *arr, N n, Args &&...rest){cerr << "[";for (N i = 0; i < n; ++i){if (i != 0)cerr << ", ";cerr << arr[i];}cerr << "]";if (sizeof...(rest) > 0)cerr << ", ";_dbg(rest...);}
+const int N = 1e5 + 10, M = 1e5 + 10;
+inline void solve()
+{
+	int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(n+1, vector<int>(n+1));
+    for (int i = 1; i <= m;i++)
+    {
+        int x,y;
+        cin >> x >> y;
+        a[x][y] = 1;
+        a[y][x] = 1;
+    }
+    vector<int>cnt(n + 1, 0);
+    for(int i = 1; i <= n;i++)
+    {
+        for (int j = 1; j <= n;j++)
+        {
+            cout<<a[i][j]<<" ";
+            if (a[i][j] == 1)
+            {
+                cnt[i]++;
+            }
+        }
+        cout << endl;
+    }
+    for(int i = 1; i <= n;i++)
+    {
+        cout << cnt[i] << " ";
+        for(int j = 1; j <= n;j++)
+        {
+            if (a[i][j] == 1)
+            {
+                cout << j << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+signed main()
+{
+    cin.tie(nullptr)->ios::sync_with_stdio(false);cout.tie(nullptr);
+#if ONLINE_JUDGE
+	char readBuffer[1 << 20];
+	cin.rdbuf()->pubsetbuf(readBuffer, sizeof(readBuffer));
+#endif
+	int T = 1;	//cin>>T;
+	while(T--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
