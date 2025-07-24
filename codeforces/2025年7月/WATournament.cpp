ae3a2f7f66d88295f6f7d25ac6e168a8792dd79e
Tick Zhong <1601858426@qq.com>
2025-07-24 09:31:44 +0800

    添加多个新算法实现，包括AtCoder公寓、几何序列、IQ测试、注册系统、WA锦标赛、请柬和全对问题

---
+++ codeforces/2025年7月/WATournament.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define gcd __gcd
+typedef int64_t i64;
+typedef int32_t i32;
+using ll = long long;using ull = unsigned long long;using LL = long long;using ULL = unsigned long long;
+using namespace std;
+const int inf = 0x3f3f3f3f,INF = 0x3f3f3f3f;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+#define int long long
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){os << "[";for (auto it = v.begin(); it != v.end(); ++it){if (it != v.begin())os << ", ";os << *it;}return os << "]";}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+#define dbg(...) cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = ", _dbg(__VA_ARGS__), cerr << endl
+void _dbg() {}
+template <typename T, typename... Args>
+void _dbg(T &&arg, Args &&...args){cerr << arg;if (sizeof...(args) > 0)cerr << ", ";_dbg(args...);}
+template <typename T, typename N, typename... Args>
+void _dbg(T *arr, N n, Args &&...rest){cerr << "[";for (N i = 0; i < n; ++i){if (i != 0)cerr << ", ";cerr << arr[i];}cerr << "]";if (sizeof...(rest) > 0)cerr << ", ";_dbg(rest...);}
+const int N = 1e5 + 10, M = 1e5 + 10;
+vector<vector<int>> g;
+inline void solve()
+{
+    int n;
+    cin >> n;
+    g.assign(n + 1, vector<int>());
+    for (int i = 1; i < n*(n-1)/2; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        g[u].pb(v);
+        g[v].pb(u);
+    }
+    vector<int> ans;
+    int tar = n - 1;
+    for (int i = 1; i <= n; i++)
+    {
+        if(g[i].size()!=tar)
+        {
+            ans.pb(i);
+        }
+    }
+    cout<<ans[0]<<" "<<ans[1]<<endl;
+}
+
+signed main()
+{
+    cin.tie(nullptr)->ios::sync_with_stdio(false);cout.tie(nullptr);
+#if ONLINE_JUDGE
+	char readBuffer[1 << 20];
+	cin.rdbuf()->pubsetbuf(readBuffer, sizeof(readBuffer));
+#endif
+	int T = 1;	//cin>>T;
+	while(T--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
