0ed12bb03f25dc5168d985a42bb61bd0c95c837c
Tick Zhong <1601858426@qq.com>
2025-07-26 15:24:46 +0800

    添加多个新算法实现，包括DSU合并、前缀和计算、任务调度和字符串比较

--- test.cpp
+++ test.cpp
@@ -23,81 +23,18 @@ struct DSU
 
 inline void solve()
 {
-    ll H, W;
-    int K;
-    cin >> H >> W >> K;
-    vector<pair<ll, ll>> obs(K);
-    obs.reserve(K);
-    for (int i = 0; i < K; i++)
-    {
-        cin >> obs[i].first >> obs[i].second;
-    }
-    // Map each obstacle coord to its index
-    unordered_map<ll, int> idx;
-    idx.reserve(K * 2);
-    auto encode = [&](ll r, ll c)
-    {
-        return (r << 32) ^ c;
-    };
-    for (int i = 0; i < K; i++)
-    {
-        idx[encode(obs[i].first, obs[i].second)] = i;
-    }
-    DSU dsu(K);
-    // DSU merge adjacent obstacles
-    for (int i = 0; i < K; i++)
-    {
-        ll r = obs[i].first, c = obs[i].second;
-        const ll dr[8] = {1, 0, -1, 0, 1, 1, -1, -1}, dc[8] = {0, 1, 0, -1, 1, -1, 1, -1};
-        for (int d = 0; d < 8; d++)
-        {
-            ll nr = r + dr[d], nc = c + dc[d];
-            ll code = encode(nr, nc);
-            auto it = idx.find(code);
-            if (it != idx.end())
-            {
-                dsu.unite(i, it->second);
-            }
-        }
-    }
-    // Track boundary touches for each component root
-    vector<bool> touchTop(K, false), touchBot(K, false);
-    vector<bool> touchLeft(K, false), touchRight(K, false);
-    for (int i = 0; i < K; i++)
-    {
-        int rt = dsu.find(i);
-        ll r = obs[i].first, c = obs[i].second;
-        if (r == 1)
-            touchTop[rt] = true;
-        if (r == H)
-            touchBot[rt] = true;
-        if (c == 1)
-            touchLeft[rt] = true;
-        if (c == W)
-            touchRight[rt] = true;
-    }
-    // Check if any component blocks path
-    bool blocked = false;
-    for (int i = 0; i < K; i++)
-    {
-        if (dsu.find(i) == i)
-        {
-            if ((touchTop[i] && touchBot[i]) || (touchLeft[i] && touchRight[i]) ||
-                (touchTop[i] && touchLeft[i]) || (touchBot[i] && touchRight[i]))
-            {
-                blocked = true;
-                break;
-            }
-        }
-    }
-    cout << (blocked ? "No\n" : "Yes\n");
+    
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    cout << *s.begin() << endl;
     solve();
     return 0;
 }
