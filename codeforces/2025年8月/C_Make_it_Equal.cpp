fb111e8ef398cdf6f98dbcc1471efc273be4976f
Tick Zhong <1601858426@qq.com>
2025-08-29 10:32:46 +0800

    添加新的 C++ 源文件，实现 C_Make_it_Equal 问题的解决方案。

---
+++ codeforces/2025年8月/C_Make_it_Equal.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define gcd __gcd
+typedef int64_t i64;
+typedef int32_t i32;
+using ll = long long;using ull = unsigned long long;using LL = long long;using ULL = unsigned long long;
+using namespace std;
+const int inf = 0x3f3f3f3f,INF = 0x3f3f3f3f;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+#define int long long
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){os << "[";for (auto it = v.begin(); it != v.end(); ++it){if (it != v.begin())os << ", ";os << *it;}return os << "]";}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+#define dbg(...) cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = ", _dbg(__VA_ARGS__), cerr << endl
+void _dbg() {}
+template <typename T, typename... Args>
+void _dbg(T &&arg, Args &&...args){cerr << arg;if (sizeof...(args) > 0)cerr << ", ";_dbg(args...);}
+template <typename T, typename N, typename... Args>
+void _dbg(T *arr, N n, Args &&...rest){cerr << "[";for (N i = 0; i < n; ++i){if (i != 0)cerr << ", ";cerr << arr[i];}cerr << "]";if (sizeof...(rest) > 0)cerr << ", ";_dbg(rest...);}
+const int N = 1e5 + 10, M = 1e5 + 10;
+inline void solve()
+{
+	int n,k;
+    cin>>n>>k;
+    vector<int> a(n),b(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++) cin>>b[i];
+	map<int,int> mp;
+    for(int i=0;i<n;i++)
+    {
+        mp[min(a[i] % k, k - a[i] % k)]++;
+    }
+    for (int i = 0; i < n;i++)
+    
+    {
+        if (mp.find(min(b[i] % k, k - b[i] % k)) == mp.end() || mp[min(b[i] % k, k - b[i] % k)] == 0)
+        {
+            cout<<"NO"<<endl;
+            return;
+        }
+        else
+        {
+            mp[min(b[i] % k, k - b[i] % k)]--;
+        }
+    }
+    cout<<"YES"<<endl;
+}
+
+signed main()
+{
+    cin.tie(nullptr)->ios::sync_with_stdio(false);cout.tie(nullptr);
+#if ONLINE_JUDGE
+	char readBuffer[1 << 20];
+	cin.rdbuf()->pubsetbuf(readBuffer, sizeof(readBuffer));
+#endif
+	int T = 1;	cin>>T;
+	while(T--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
