09195c8f165dcde536a89d74970b35eb3037dae9
Tick Zhong <1601858426@qq.com>
2025-11-05 19:30:27 +0800

    添加多个新的 C++ 源文件，包含子网格计数、卡车司机问题、子段乘积、括号序列和欧几里得算法的实现，重构代码结构。

---
+++ atcoder/AtCoder Beginner Contest 430/B_Count_Subgrid.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define gcd __gcd
+typedef int64_t i64;typedef int32_t i32;using ll = long long;using ull = unsigned long long;using LL = long long;using ULL = unsigned long long;
+using std::cout, std::cin, std::cerr ,std::vector, std::string, std::pair, std::map, std::set, std::priority_queue, std::queue, std::stack, std::sort, std::unordered_map, std::unordered_set, std::min, std::max, std::sort, std::reverse, std::swap, std::abs, std::ostream, std::to_string, std::lower_bound, std::upper_bound;
+constexpr int inf = 0x3f3f3f3f;
+constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+void test(){cout<<"test"<<endl;}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){os << "[";for (auto it = v.begin(); it != v.end(); ++it){if (it != v.begin())os << ", ";os << *it;}return os << "]";}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+#define dbg(...) cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = ", _dbg(__VA_ARGS__), cerr << endl
+void _dbg() {}
+template <typename T, typename... Args>
+void _dbg(T &&arg, Args &&...args){cerr << arg;if (sizeof...(args) > 0)cerr << ", ";_dbg(args...);}
+template <typename T, typename N, typename... Args>
+void _dbg(T *arr, N n, Args &&...rest){cerr << "[";for (N i = 0; i < n; ++i){if (i != 0)cerr << ", ";cerr << arr[i];}cerr << "]";if (sizeof...(rest) > 0)cerr << ", ";_dbg(rest...);}
+#define int long long
+constexpr int N = 1e5 + 10, M = 1e5 + 10;
+inline void solve()
+{
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int>> grid(n+1, vector<int>(n+1));
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            char x;
+            cin >> x;
+            if(x=='#')
+            {
+                grid[i][j] = 1;
+            }
+            else
+            {
+                grid[i][j] = 0;
+            }
+        }
+    }
+    set<vector<vector<int>>> st;
+    for(int i = 1; i <= n - m + 1; i++)
+    {
+        for(int j = 1; j <= n - m + 1; j++)
+        {
+            vector<vector<int>> subgrid;
+            for(int x = 0; x < m; x++)
+            {
+                vector<int> row;
+                for(int y = 0; y < m; y++)
+                {
+                    row.pb(grid[i + x][j + y]);
+                }
+                subgrid.pb(row);
+            }
+            st.insert(subgrid);
+        }
+    }
+    cout<<st.size()<<endl;
+}
+
+signed main()
+{
+    cin.tie(nullptr)->std::ios::sync_with_stdio(false);cout.tie(nullptr);
+#if ONLINE_JUDGE
+	char readBuffer[1 << 20];
+	cin.rdbuf()->pubsetbuf(readBuffer, sizeof(readBuffer));
+#endif
+	int T = 1;	
+    // cin>>T;
+	while(T--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
