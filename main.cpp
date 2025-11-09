9df87c55ac91e6fbebe02f41f552e21940b19929
Tick Zhong <1601858426@qq.com>
2025-11-09 15:37:58 +0800

    添加多个新的 C++ 源文件，包含机器人重量、机器人工厂、机器人定制和子段异或的实现，重构代码结构。

--- main.cpp
+++ main.cpp
@@ -1,36 +1,109 @@
 #include <bits/stdc++.h>
 #define endl "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define gcd __gcd
+typedef int64_t i64;
+typedef int32_t i32;
 using ll = long long;
 using ull = unsigned long long;
-// using namespace std;
-const int inf = 0x3f3f3f3f;
-struct DSU {
-    std::vector<int> p, r;
-    DSU(int n) : p(n + 1), r(n + 1, 0) {
-        std::iota(p.begin(), p.end(), 0);
+using LL = long long;
+using ULL = unsigned long long;
+using std::cout, std::cin, std::cerr, std::vector, std::string, std::pair, std::map, std::set, std::priority_queue, std::queue, std::stack, std::sort, std::unordered_map, std::unordered_set, std::min, std::max, std::sort, std::reverse, std::swap, std::abs, std::ostream, std::to_string, std::lower_bound, std::upper_bound, std::deque;
+constexpr int inf = 0x3f3f3f3f;
+constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+void test() { cout << "test" << endl; }
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
     }
-	int find(int x) {
-		return p[x] == x ? x : p[x] = find(p[x]);
-	}
-	bool unite(int a, int b) {
-		a = find(a);
-		b = find(b);
-		if (a == b)
-			return false;
-		if (r[a] < r[b])
-            std::swap(a, b);
-        p[b] = a;
-		if (r[a] == r[b])
-			r[a]++;
-		return true;
-	}
+    return os << "]";
+}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+#define dbg(...) cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = ", _dbg(__VA_ARGS__), cerr << endl
+void _dbg() {}
+template <typename T, typename... Args>
+void _dbg(T &&arg, Args &&...args)
+{
+    cerr << arg;
+    if (sizeof...(args) > 0)
+        cerr << ", ";
+    _dbg(args...);
+}
+template <typename T, typename N, typename... Args>
+void _dbg(T *arr, N n, Args &&...rest)
+{
+    cerr << "[";
+    for (N i = 0; i < n; ++i)
+    {
+        if (i != 0)
+            cerr << ", ";
+        cerr << arr[i];
+    }
+    cerr << "]";
+    if (sizeof...(rest) > 0)
+        cerr << ", ";
+    _dbg(rest...);
+}
+#define int long long
+constexpr int N = 1e5 + 10, M = 1e5 + 10;
+ll C(int n, int m)
+{
+    if (m > n)
+        return 0;
+    ll res = 1;
+    for (int i = 1; i <= m; i++)
+    {
+        res = res * (n - i + 1) / i;
+    }
+    return res;
 };
+inline void solve()
+{
+    int n;
+    cin >> n;
+    unordered_set<int> s;
+    for( int i = 0; i < n; ++i )
+    {
+        int x;
+        cin >> x;
+        if(!s.contains(x))
+        {
+            s.insert(x);
+            cout << x << " ";
+        }
+    }
+}
+
 signed main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr), std::cout.tie(nullptr);
-    int n;
-    std::cin >> n;
+    cin.tie(nullptr)->std::ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+#if ONLINE_JUDGE
+    char readBuffer[1 << 20];
+    cin.rdbuf()->pubsetbuf(readBuffer, sizeof(readBuffer));
+#endif
+    int T = 1;
+    // cin>>T;
+    while (T--)
+    {
+        solve();
+    }
 
     return 0;
 }
