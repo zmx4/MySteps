d60313f90bd5b3d5c71d505f4a94e25a9207c983
Tick Zhong <1601858426@qq.com>
2026-02-07 15:23:46 +0800

    2026牛客寒假算法基础集训营3

---
+++ nowcoder/牛客寒假算法基础集训营/2026牛客寒假算法基础集训营3/Tic_Tac_DREAMIN.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstdint>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <cstring>
+#include <climits>
+#include <iterator>
+#include <bitset>
+#include <numeric>
+#define endl "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define gcd __gcd
+typedef int64_t i64;typedef int32_t i32;using ll = long long;using ull = unsigned long long;using LL = long long;using ULL = unsigned long long;using std::cout, std::bitset, std::cin, std::cerr, std::vector, std::string, std::pair, std::map, std::set, std::priority_queue, std::queue, std::stack, std::sort, std::unordered_map, std::greater,std::unordered_set, std::min, std::max, std::reverse, std::swap, std::abs, std::ostream, std::to_string, std::lower_bound, std::upper_bound, std::deque, std::iota;
+constexpr int inf = 0x3f3f3f3f;constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+void test(){cout<<"test"<<endl;}
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){os << "[";for (auto it = v.begin(); it != v.end(); ++it){if (it != v.begin())os << ", ";os << *it;}return os << "]";}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+#define dbg(...) cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = ", _dbg(__VA_ARGS__), cerr << endl
+void _dbg() {}
+template <typename T, typename... Args>
+void _dbg(T &&arg, Args &&...args){cerr << arg;if (sizeof...(args) > 0)cerr << ", ";_dbg(args...);}
+template <typename T, typename N, typename... Args>
+void _dbg(T *arr, N n, Args &&...rest){cerr << "[";for (N i = 0; i < n; ++i){if (i != 0)cerr << ", ";cerr << arr[i];}cerr << "]";if (sizeof...(rest) > 0)cerr << ", ";_dbg(rest...);}
+#include <cmath>
+#include <format>
+#pragma region solve
+#define int long long
+namespace solve
+{
+double xa, ya, xb, yb;
+bool check(double x,double y = 0)
+{
+    double a = sqrt((xa - x) * (xa - x) + (ya - y) * (ya - y));
+    double b = sqrt((xb - x) * (xb - x) + (yb - y) * (yb - y));
+    double c = sqrt((xa - xb) * (xa - xb) + (ya - yb) * (ya - yb));
+    double p = (a + b + c) / 2;
+    double s = sqrt(p * (p - a) * (p - b) * (p - c));
+    return s >= 2;
+}
+constexpr int N = 1e5 + 10, M = 1e5 + 10;
+inline void Tick()
+{
+    cin >> xa >> ya;
+    cin >> xb >> yb;
+    double l = 0, r = max(xa,xb);
+    while(r - l > 1e-6)
+    {
+        double mid = (l + r) / 2;
+        if(check(mid))
+        {
+            r = mid;
+        }
+        else
+        {
+            l = mid;
+        }
+    }
+    cout << std::format("{:.4f}",l)<< endl;
+}
+}
+#pragma endregion
+
+#pragma region main
+signed main()
+{
+    cin.tie(nullptr)->std::ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    // cin.exceptions(cin.failbit | cin.badbit);
+#if ONLINE_JUDGE
+    char readBuffer[1 << 20];
+    cin.rdbuf()->pubsetbuf(readBuffer, sizeof(readBuffer));
+#endif
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve::Tick();
+    }
+
+    return 0;
+}
+#pragma endregion
--- test.cpp
+++ test.cpp
@@ -1,53 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+
 using namespace std;
-using ll = long long;
 
-struct DSU
+void solve()
 {
-    vector<int> p, r;
-    DSU(int n) : p(n + 1), r(n + 1, 0)
-    {
-        iota(p.begin(), p.end(), 0);
-    }
-    int find(int x)
-    {
-        return p[x] == x ? x : p[x] = find(p[x]);
-    }
-    bool unite(int a, int b)
-    {
-        a = find(a);
-        b = find(b);
-        if (a == b)
-            return false;
-        if (r[a] < r[b])
-            swap(a, b);
-        p[b] = a;
-        if (r[a] == r[b])
-            r[a]++;
-        return true;
-    }
-    void merge(int x, int y)
-    {
-        x = find(x);
-        y = find(y);
-        p[x] = y;
-    }
-};
-
-inline void solve()
-{
+    
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    set<int> s;
-    s.insert(1);
-    s.insert(2);
-    s.insert(3);
-    cout << *s.begin() << endl;
-    cout << *s.rbegin() << endl;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     solve();
     return 0;
 }
