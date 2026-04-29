9b88c22aee6fbc23d14f31d69fdd81739a86ec08
Tick Zhong <1601858426@qq.com>
2026-04-30 06:45:10 +0800

    add:DSU

---
+++ Others/Vjudge/[进阶并查集]/Graph_Composition.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <cstdint>
+#include <map>
+#include <algorithm>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <cstring>
+#include <climits>
+#include <iterator>
+#include <bitset>
+#include <numeric>
+#include <array>
+#define endl "\n"
+#define fi first
+#define se second
+#define pb push_back
+#define gcd __gcd
+typedef int64_t i64;typedef int32_t i32;using ll = long long;using ull = unsigned long long;using LL = long long;using ULL = unsigned long long;using std::cout, std::bitset, std::cin, std::cerr, std::vector, std::string, std::pair, std::map, std::set, std::priority_queue, std::queue, std::stack, std::sort, std::unordered_map, std::greater,std::unordered_set, std::min, std::max, std::reverse, std::swap, std::abs, std::ostream, std::to_string, std::lower_bound, std::upper_bound, std::deque, std::iota, std::array;
+constexpr int inf = 0x3f3f3f3f;constexpr long long INF = 0x3f3f3f3f3f3f3f3f;
+ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+void test(){cout<<"test"<<endl;}
+ll log2(ll x) { return 63 - __builtin_clzll(x); }
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){os << "[";for (auto it = v.begin(); it != v.end(); ++it){if (it != v.begin())os << ", ";os << *it;}return os << "]";}
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) { return os << "(" << p.first << ", " << p.second << ")"; }
+#define dbg(...) cerr << "[" << __LINE__ << "] " << #__VA_ARGS__ << " = ", _dbg(__VA_ARGS__), cerr << endl
+void _dbg() {}
+template <typename T, typename... Args>
+void _dbg(T &&arg, Args &&...args){cerr << arg;if (sizeof...(args) > 0)cerr << ", ";_dbg(args...);}
+template <typename T, typename N, typename... Args>
+void _dbg(T *arr, N n, Args &&...rest){cerr << "[";for (N i = 0; i < n; ++i){if (i != 0)cerr << ", ";cerr << arr[i];}cerr << "]";if (sizeof...(rest) > 0)cerr << ", ";_dbg(rest...);}
+#pragma region solve
+#define int long long
+namespace solve
+{
+constexpr int N = 1e5 + 10, M = 1e5 + 10;
+struct DSU {
+    vector<int> p, r;
+    DSU(int n) : p(n + 1), r(n + 1, 0) {
+        iota(p.begin(), p.end(), 0);
+    }
+    int find(int x) {
+        return p[x] == x ? x : p[x] = find(p[x]);
+    }
+    bool unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b)
+            return false;
+        if (r[a] < r[b])
+            swap(a, b);
+        p[b] = a;
+        if (r[a] == r[b])
+            r[a]++;
+        return true;
+    }
+    void merge(int x, int y)
+    {
+    x = find(x);y = find(y);
+    if(x == y)
+    {
+        return;
+    }
+    p[x] = y;
+    }
+};
+inline void Tick()
+{
+    int n, m1, m2;
+    cin >> n >> m1 >> m2;
+    DSU dsu1(2*n), dsu2(2*n);
+    vector<int> a(m1 + 1), b(m1 + 1);
+    vector<int> c(m2 + 1), d(m2 + 1);
+    for (int i = 1; i <= m1; ++i)
+    {
+        cin >> a[i] >> b[i];
+    }
+    for (int i = 1; i <= m2; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        c[i] = u;d[i] = v;
+        dsu2.merge(u, v);
+    }
+    int ans = 0;
+    for (int i = 1; i <= m1;++i)
+    {
+        int u1 = a[i], v1 = b[i];
+        int fu1 = dsu1.find(u1), fv1 = dsu1.find(v1);
+        int fu2 = dsu2.find(u1), fv2 = dsu2.find(v1);
+        if( fu2 == fv2)
+        {
+            if (fu1 != fv1)
+            {
+                dsu1.merge(u1, v1); // 在第二张图中也连通时，才真正在第一张图中合并他们
+            }
+        }
+        else
+        {
+            ans++;
+        }
+    }
+    for (int i = 1; i <= m2;++i)
+    {
+        int u2 = c[i], v2 = d[i];
+        int fu1 = dsu1.find(u2), fv1 = dsu1.find(v2);
+        int fu2 = dsu2.find(u2), fv2 = dsu2.find(v2);
+        if(fu1 != fv1)
+        {
+            ans++;
+            dsu1.merge(u2, v2);
+        }
+        
+    }
+    cout << ans << endl;
+}
+}
+#pragma endregion
+
+#pragma region main
+signed main()
+{
+    cin.tie(nullptr)->std::ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    // cin.exceptions(cin.failbit | cin.badbit);
+#if ONLINE_JUDGE
+    char readBuffer[1 << 20];
+    cin.rdbuf()->pubsetbuf(readBuffer, sizeof(readBuffer));
+#endif
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve::Tick();
+    }
+
+    return 0;
+}
+#pragma endregion
