0a87c1656caae51cd3ca03a26eaae9335f5a8c73
Tick Zhong <1601858426@qq.com>
2026-08-01 10:17:13 +0800

    add:graph

--- test.cpp
+++ test.cpp
@@ -1,58 +1,65 @@
 #include <bits/stdc++.h>
-#include <algorithm>
 using namespace std;
 #define int long long
-// #define endl '\n'
-const int mod = 998244353;
-typedef struct
-{
-    int l, r, d;
-} S;
-bool cmp(int a, int b)
-{
-    return a > b;
-}
-// int a[110];
-vector<int> a(110);
-int n, h;
 
-bool can(int k)
-{
-    int sum = 0;
-    for (int i = 2; i <= n; i++)
-    {
-        sum += min(k, a[i] - a[i - 1]);
-    }
-    sum += k;
-    return sum >= h;
-}
 signed main()
 {
-    // ios::sync_with_stdio(false);
-    // cin.tie(nullptr);
-    // cout.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int t;
     cin >> t;
-    
     while (t--)
     {
-        cin >> n >> h;
-        for (int i = 1; i <= 110; i++)
-            a[i] = 0;
+        int n;
+        cin >> n;
+        
+            vector<int> a(n + 1, 0);
+        bool alln = 1;
+        int p = 0;
         for (int i = 1; i <= n; i++)
-            cin >> a[i];
-            // ranges::sort(a.begin() + 1, a.begin() + n + 1, cmp);
-        int sum = 0;
-        int l = 1, r = h;
-        while (l <= r)
         {
-            int mid = (l + r) / 2;
-            if (can(mid))
-                r = mid - 1;
-            else
-                l = mid + 1;
+            cin >> a[i];
+            if (a[i] > 0)
+                alln = 0;
         }
-        cout << l << endl;
+        if (n == 1)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            if (a[i] > 0)
+            {
+                p = i;
+                break;
+            }
+        }
+        if (alln)
+        {
+            cout << n - 1 << endl;
+            for (int i = n - 1; i >= 1; i--)
+            {
+                cout << i << " " << i + 1 << endl;
+            }
+            continue;
+        }
+        cout << 5 + 2 * (n - 1) << endl;
+        for (int i = 1; i <= 5; i++)
+            cout << p << " " << p << endl;
+        
+        for (int i = 2; i <= n - 1; i++)
+        {
+            for (int j = 1; j <= 2; j++)
+                cout << i + 1 << " " << i << endl;
+        }
     }
     return 0;
 }
+/*
+
+1 -19 -19 
+
+
+
+*/
